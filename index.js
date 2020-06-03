01e3b9de26bfe44f178ffd45a65f8aff9a30af49
Beekasha <brian.j.kleiner@gmail.com>
2020-06-03 01:20:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    let thankYouCards = []
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouCards;
+}
+
+function countDown(num) {
+    while (num > 0) {
+        console.log(num);
+        num -= 1;
+    }
+    console.log(num);
+}
