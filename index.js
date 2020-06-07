57b7f93c8945b7c95db32fff16090ebec9d4209b
marsha <marshaannon@gmail.com>
2020-06-06 21:21:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    let message = []
+    for (let i = 0; i < name.length; i++) {
+        message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return message;
+}
+
+function countDown(num) {
+    while ( num > 0) {
+        console.log(num);
+        num -=1;
+    }
+    console.log(num);
+}
