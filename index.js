d2a40de1b96b2ade784ca374746698bb1276604a
matt_kuras <mattkuras@gmail.com>
2020-06-09 19:12:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+ 
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+ 
+  return gifts;
+}
+
+function writeCards(array, event) {
+    let thankyouCards = []
+    for (let i = 0; i < array.length; i++) {
+        thankyouCards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankyouCards
+}
+
+function countDown(count) {
+    let i = 0; 
+    while (i <= count){
+        console.log(i)
+        i ++
+    }
+}
