1d4770d6df25a4fd92cdcc1be8905872f64f802b
Dusch4593 <brandondusch@gmail.com>
2020-06-09 12:36:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let writeCards = (names, event_name) => {
+  let thankYouMessages = []
+  for(let i = 0; i < names.length; i++) {
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event_name} gift!`)
+  }
+  return thankYouMessages
+}
+
+
+let countDown = (number) => {
+  let next_number = number
+  while(next_number >= 0) {
+    console.log(next_number);
+    next_number--;
+  }
+}
