cf86b9eccfb71f77f39073be2894985737157c14
Anil <anil_gurung15@yahoo.com>
2020-06-20 22:41:57 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function countDown(number) {
+    while (number > -1) {
+        console.log(number)
+        number--;
+    }
+
+}
+
+function writeCards(namesArray, eventName) {
+    let thank_you_messages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        thank_you_messages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`)
+
+    }
+    return thank_you_messages;
+}
